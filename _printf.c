33900fda5c8754dafefea20f74f3ad01ca68fe6c
Garrick Larson Potts <ricki.glp@gmail.com>
2023-08-18 09:37:47 +0000

    Created the printf base file.

---
+++ _printf.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _printf - Prints a formated string to the stdout.
+ * @format: Const string to be printed to output.
+ *
+ * Return: Number of charaters printed.
+ */
+
+int _printf(const char *format, ...)
+{
+	unsigned int count;
+	va_list f_spec;
+
+	return (count);
+}
