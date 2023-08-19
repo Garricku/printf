069bc64930983ffd979a986c2a16196fac6db942
Umar Raji Bello <umarrajibello@gmail.com>
2023-08-19 11:21:51 +0000

    Added a basic case for specifiers

--- _printf.c
+++ _printf.c
@@ -9,14 +9,30 @@
 
 int _printf(const char *format, ...)
 {
+	/* count to store count chars printed, i for iteration */
 	unsigned int count, i;
 	va_list f_spec;
+	/*function pointer to receive function for printing specific case*/
+	int (*fptr)(va_list);
+
+	if (format == NULL)
+		return (-1);
 
 	va_start(f_spec, format);
-	for(i = 0; format[i]; i++)
+	for (i = 0; format[i]; i++)
 	{
 		if (format[i] != '%')
 			count += write(1, (format + i), 1);
+		else
+		{
+			if (format[i + 1] == '\0')
+				return (-1);
+			/*get appropriate function for specified format specifier*/
+			fptr = select_fun(format[i + 1]);
+			if (fptr == NULL)
+				return (-1);
+			/*prints, return no. of cars printed and adds it to count variable*/
+			count += fptr(f_spec);
 	}
 
 
