d50863c3cd6992fd24cf5e006de51d87ef3312e6
Umar Raji Bello <umarrajibello@gmail.com>
2023-08-19 10:41:03 +0000

    First simple test

--- _printf.c
+++ _printf.c
@@ -9,8 +9,18 @@
 
 int _printf(const char *format, ...)
 {
-	unsigned int count;
+	unsigned int count, i;
 	va_list f_spec;
 
+	va_start(f_spec, format);
+	for(i = 0; format[i]; i++)
+	{
+		if (format[i] != '%')
+			count += write(1, (format + i), 1);
+	}
+
+
+
+	va_end(f_spec);
 	return (count);
 }
