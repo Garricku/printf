ccddfe175540a4b80b52b49f844a3c154d74377f
Garrick Larson Potts <ricki.glp@gmail.com>
2023-08-22 10:17:37 +0000

    created an atoi function

---
+++ to_be_tested/a_to_i.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* a_to_i - A atoi function assci to integer.
+ * @c: The char to be changed to an integer.
+ *
+ * Return: The converted interger.
+ */
+
+int a_to_i(char c)
+{
+	/*tought we might need this for creating the adresses*/
+	char c = 'a';
+	int i = (int)c;
+	return (c);
+}
